b5fff13f20e8b7a1bd145a11b772a0f09c0bb89c
lx0128 <1358432408@qq.com>
2019-03-21 14:46:22 +0800

    修改密码重复注册

--- catalog/controller/account/login.php
+++ catalog/controller/account/login.php
@@ -295,6 +295,14 @@ class ControllerAccountLogin extends Controller {
 	        $json['error']['agree'] = "Warning: You must agree to the registration agreement!";
 	    }
 
+	    $res = $this->makeRequest('https://opencart.com/index.php?route=account/login/getcustomerbytotal'.'&email='.$this->request->post['email']);
+	    // $reqData = json_decode($res['result'], true);
+	   // print_r();
+	   // exit;
+	    if($res['result']==1){
+	    	$json['error']['warning'] = $this->language->get('error_exists');
+	    }
+	    // print_r($json['error']['warning']);exit;
 		// Agree to terms
 		if ($this->config->get('config_account_id')) {
 			$this->load->model('catalog/information');
@@ -335,6 +343,82 @@ class ControllerAccountLogin extends Controller {
 		$this->response->addHeader('Content-Type: application/json');
 		$this->response->setOutput(json_encode($json));
 	}
+
+
+/**
+ * 发起http请求
+ * @param string $url 访问路径
+ * @param array $params 参数，该数组多于1个，表示为POST
+ * @param int $expire 请求超时时间
+ * @param array $extend 请求伪造包头参数
+ * @param string $hostIp HOST的地址
+ * @return array    返回的为一个请求状态，一个内容
+ */
+function makeRequest($url, $expire = 0, $extend = array(), $hostIp = '')
+{
+
+
+
+    if (empty($url)) {
+        return array('code' => '100');
+    }
+    // $param=json_encode($params);
+    // print_r($param);exit;
+    $_curl = curl_init();
+    $_header =array(
+              	'Accept-Language: zh-CN',
+		        'Connection: Keep-Alive',
+		        'Cache-Control: no-cache'
+           );
+    // 方便直接访问要设置host的地址
+    if (!empty($hostIp)) {
+        $urlInfo = parse_url($url);
+        if (empty($urlInfo['host'])) {
+            $urlInfo['host'] = substr(DOMAIN, 7, -1);
+            $url = "http://{$hostIp}{$url}";
+        } else {
+            $url = str_replace($urlInfo['host'], $hostIp, $url);
+        }
+        $_header[] = "Host: {$urlInfo['host']}";
+    }
+
+    // if (!empty($param)) {
+    //     curl_setopt($_curl, CURLOPT_POSTFIELDS, $param);
+    //     curl_setopt($_curl, CURLOPT_POST, true);
+    // }
+    if (substr($url, 0, 8) == 'https://') {
+        curl_setopt($_curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($_curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+    }
+    curl_setopt($_curl, CURLOPT_URL, $url);
+    curl_setopt($_curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($_curl, CURLOPT_USERAGENT, 'API PHP CURL');
+    curl_setopt($_curl, CURLOPT_HTTPHEADER, $_header);
+
+    if ($expire > 0) {
+        curl_setopt($_curl, CURLOPT_TIMEOUT, $expire); // 处理超时时间
+        curl_setopt($_curl, CURLOPT_CONNECTTIMEOUT, $expire); // 建立连接超时时间
+    }
+
+    // 额外的配置
+    if (!empty($extend)) {
+        curl_setopt_array($_curl, $extend);
+    }
+    $result['result'] = curl_exec($_curl);
+    $result['code'] = curl_getinfo($_curl, CURLINFO_HTTP_CODE);
+    $result['info'] = curl_getinfo($_curl);
+    if ($result['result'] === false) {
+        $result['result'] = curl_error($_curl);
+        $result['code'] = -curl_errno($_curl);
+    }
+    // print_r($result);exit;
+    curl_close($_curl);
+    return $result;
+}
+
+
+
+
 
 
 }
